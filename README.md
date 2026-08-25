fc7bcf1cd3a0130bf80ee367506c7fda2828fdb2
Pedro Pachu <pedropachununes@gmail.com>
2026-08-25 07:59:37 -0300

    Validação das curvas

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Funções primárias responsáveis pelo funcionamento e movimentação do robô.
 | voltar() | Gira o robo no proprio eixo 180º| - | -|
 | girar() | Gira o robo no angulo desejado | ```Angulo``` | - 
 | virar() | Faz uma leve curva sem parar o robo para o lado desejado | ```lado ('E') ou ('D') ``` | -
-| virar90 | Realiza uma curva de 90º para o lado desejado | ``` lado ('E') ou ('D')  ```| -
+| virar_2() | Faz uma leve curva sem parar o robo para o lado desejado, usada para ajustes finos| ```lado ('E') ou ('D') ``` | -
 | desviar_obstaculo() | Executa o procedimento de desvio | - | -
 | acelaracao_por_angulo() | Acelera o robo com base no angulo dos censores | ``` velocidade_desejada ```| ```velocidade``` 
 
@@ -20,12 +20,14 @@ Funções primárias responsáveis pelo funcionamento e movimentação do robô.
 |:---| ---|
 | **me** | Motor Esquerdo
 | **md** | Motor Direito
-| **sce** | Sensor de cor Esquerda
+| **sce** | Sensor de cor Esquerdo
 | **scd** | Sensor de cor Direito
+| **scdt** | Sensor de cor Direito Traseiro
+| **scd** | Sensor de cor Esquerdo Traseiro
 | **scm** | Sensor de cor Central
-| **sudt** | Sensor Ultrasônico Direita Traseito
+| **sudt** | Sensor Ultrasônico Direita Traseiro
 | **sudf** | Sensor Ultrasônico Direita Frontal
-| **suet** | Sensor Ultrasônico Esquerda Traseito
+| **suet** | Sensor Ultrasônico Esquerda Traseiro
 | **suef** | Sensor Ultrasônico Esquerda Frontal
 | **suf**  | Sensor Ultrasônico Frontal
 | **sub**  | Sensor Ultrasônico Inferior 
--- src/teste01.cs
+++ src/teste01.cs
@@ -2,74 +2,130 @@
 string motor_esquerda_ref = "me";
 string motor_direita_ref = "md";
 string sensor_cor_esquerda_ref = "sce";
+string sensor_cor_esquerda_tras_ref = "scet";
 string sensor_cor_meio_ref = "scm";
 string sensor_cor_direita_ref = "scd";
+string sensor_cor_direita_tras_ref = "scdt";
 
 
 // main functions
 
-async Task andar_frente(double velocidade = 100) {
+async Task andar_frente(double velocidade = 100)
+{
     Bot.GetComponent<Servomotor>(motor_direita_ref).Locked = false; // Destrava o motor da direita
     Bot.GetComponent<Servomotor>(motor_esquerda_ref).Locked = false; // Destrava o motor da esquerda
     Bot.GetComponent<Servomotor>(motor_direita_ref).Apply(Math.Abs(velocidade), velocidade); //  velocidade de rotação e torque
     Bot.GetComponent<Servomotor>(motor_esquerda_ref).Apply(Math.Abs(velocidade), velocidade); // velocidade de rotação e torque
 }
-async Task virar_esquerda(double velocidade = 200, double tick = 1) {
-    Bot.GetComponent<Servomotor>(motor_esquerda_ref).Apply(1000, -700);
+async Task virar_esquerda(double velocidade = 200, double tick = 10)
+{
+    Bot.GetComponent<Servomotor>(motor_esquerda_ref).Apply(1000, -800);
     // Bot.GetComponent<Servomotor>(motor_esquerda_ref).Locked = true; // Trava o motor da esquerda
     Bot.GetComponent<Servomotor>(motor_direita_ref).Locked = false; // Destrava o motor da direita
     Bot.GetComponent<Servomotor>(motor_direita_ref).Apply(Math.Abs(velocidade * 2), velocidade * 2); //*
     await Time.Delay(tick);
 }
-        //---------mesclar o virar_direita e o virar_esquerda ele deve receber o parametro se é direita ou esquerda-------------------------
-async Task virar_direita(double velocidade = 200, double tick = 1) {
-    Bot.GetComponent<Servomotor>(motor_direita_ref).Apply(1000, -700);
+//---------mesclar o virar_direita e o virar_esquerda ele deve receber o parametro se é direita ou esquerda-------------------------
+async Task virar_direita(double velocidade = 200, double tick = 10)
+{
+    Bot.GetComponent<Servomotor>(motor_direita_ref).Apply(1000, -800);
     // Bot.GetComponent<Servomotor>(motor_direita_ref).Locked = true; // Trava o motor da direita
     Bot.GetComponent<Servomotor>(motor_esquerda_ref).Locked = false; // Destrava o motor da esquerda
     Bot.GetComponent<Servomotor>(motor_esquerda_ref).Apply(Math.Abs(velocidade * 2), velocidade * 2);
     await Time.Delay(tick);
 }
-async Task volta(double velocidade = 100) { //----------------------ele deveria virar 180°-----------------
+
+async Task virar_2(double velocidade = 200, double tick = 1, string lado = "D")
+{
+    if ((lado.ToString() == "E"))
+    {
+        IO.PrintLine("Esquerda");
+        Bot.GetComponent<Servomotor>(motor_direita_ref).Apply(1000, -00);
+        Bot.GetComponent<Servomotor>(motor_esquerda_ref).Locked = false; // Destrava o motor da esquerda
+        Bot.GetComponent<Servomotor>(motor_esquerda_ref).Apply(Math.Abs(velocidade * 2), velocidade * 2);
+        await Time.Delay(tick);
+    }
+    if ((lado.ToString() == "D"))
+    {
+        IO.PrintLine("Direita");
+        Bot.GetComponent<Servomotor>(motor_esquerda_ref).Apply(1000, -700);
+        Bot.GetComponent<Servomotor>(motor_direita_ref).Locked = false; // Destrava o motor da direita
+        Bot.GetComponent<Servomotor>(motor_direita_ref).Apply(Math.Abs(velocidade * 2), velocidade * 2); //*
+        await Time.Delay(tick);
+    }
+}
+
+async Task volta(double velocidade = 100)
+{ //----------------------ele deveria virar 180°-----------------
     Bot.GetComponent<Servomotor>(motor_direita_ref).Locked = false; // Destrava o motor da direita
     Bot.GetComponent<Servomotor>(motor_esquerda_ref).Locked = false; // Destrava o motor da esquerda
     Bot.GetComponent<Servomotor>(motor_direita_ref).Apply(Math.Abs(0 - velocidade), 0 - velocidade); // 
     Bot.GetComponent<Servomotor>(motor_esquerda_ref).Apply(Math.Abs(0 - velocidade), 0 - velocidade); // 
 }
 
-async Task travar_motor() { // 
-    Bot.GetComponent<Servomotor>(motor_direita_ref).Locked = true ; // Trava o motor da direita
-    Bot.GetComponent<Servomotor>(motor_esquerda_ref).Locked = true ; // Trava o motor da esquerda
+async Task travar_motor()
+{ // 
+    Bot.GetComponent<Servomotor>(motor_direita_ref).Locked = true; // Trava o motor da direita
+    Bot.GetComponent<Servomotor>(motor_esquerda_ref).Locked = true; // Trava o motor da esquerda
 }
 
 
 // main task
 
-async Task Main() {
-    while (true) {
+async Task Main()
+{
+    IO.OpenConsole();
+    while (true)
+    {
         await Time.Delay(0.2); // pro treco nao explodir, anti kaboom 3000
-        
+
         if (
             ((Bot.GetComponent<ColorSensor>(sensor_cor_direita_ref).Analog).ToString() == "Preto") && //detectar se nececita de virar a esquerda
             ((Bot.GetComponent<ColorSensor>(sensor_cor_esquerda_ref).Analog).ToString() != "Preto")
-        ) {
+        )
+        {
             await virar_direita(1000);
-    
-        } else if (
+
+        }
+        else if (
             ((Bot.GetComponent<ColorSensor>(sensor_cor_direita_ref).Analog).ToString() != "Preto") && //detectar se nececita de virar a direita
             ((Bot.GetComponent<ColorSensor>(sensor_cor_esquerda_ref).Analog).ToString() == "Preto")
-        ) {
+        )
+        {
             await virar_esquerda(1000);
-        } else if (
+        }
+        else if (
             ((Bot.GetComponent<ColorSensor>(sensor_cor_direita_ref).Analog).ToString() == "Preto") && //detecta se está tudo bem seguir em frente
             ((Bot.GetComponent<ColorSensor>(sensor_cor_esquerda_ref).Analog).ToString() == "Preto")
-        ) {
+        )
+        {
             await andar_frente();
-        } else if (
+        }
+        else if (
+            ((Bot.GetComponent<ColorSensor>(sensor_cor_direita_tras_ref).Analog).ToString() == "Preto") && //detectar se nececita de virar a esquerda
+            ((Bot.GetComponent<ColorSensor>(sensor_cor_esquerda_tras_ref).Analog).ToString() != "Preto")
+        )
+        {
+            IO.PrintLine("Esquerda");
+            await virar_2(1000, 1000, "E");
+        }
+        else if (
+            ((Bot.GetComponent<ColorSensor>(sensor_cor_direita_tras_ref).Analog).ToString() != "Preto") && //detectar se nececita de virar a direita
+            ((Bot.GetComponent<ColorSensor>(sensor_cor_esquerda_tras_ref).Analog).ToString() == "Preto")
+        )
+        {
+            IO.PrintLine("Direita");
+            await virar_2(1000, 1000, "D");
+        }
+        else if (
             ((Bot.GetComponent<ColorSensor>(sensor_cor_direita_ref).Analog).ToString() == "Vermelho") || //parar na linha de chegada
             ((Bot.GetComponent<ColorSensor>(sensor_cor_esquerda_ref).Analog).ToString() == "Vermelho")
-        ) {
+        )
+        {
             await travar_motor();
-        } else {
+        }
+        else
+        {
             await andar_frente(200); //se não parar andar pra frente
         }
     }
